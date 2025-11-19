b66e3ed22cf9663a8bb837cdec008350d2e899f0
StepanSaribekov6 <m.saribekov6@mail.ru>
2025-11-19 14:15:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @param {string} name 
+ * @param {string} age 
+ * @returns {string}
+ */
+
+function hello(name, age) {
+    return `Hello, ${name}! Your age: ${age}`; // Ввод имени и возраста
+}
+
+console.log(hello("Max", 19)) // Вывод имени и возраста - приветствие
